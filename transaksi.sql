15ee264fd60af9c1bd8d6893d97c51165a1cebf2
sabitahraya <sabitahraya0@gmail.com>
2026-06-23 11:06:10 +0700

    Tambah file database

---
+++ transaksi.sql
@@ -0,0 +1,85 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 23, 2026 at 06:02 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `cashflow`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaksi`
+--
+
+CREATE TABLE `transaksi` (
+  `id` int(11) NOT NULL,
+  `bank_id` int(11) NOT NULL,
+  `tipe` enum('pemasukan','pengeluaran') NOT NULL,
+  `jumlah` decimal(15,2) NOT NULL,
+  `deskripsi` text NOT NULL,
+  `tanggal_transaksi` date NOT NULL,
+  `tanggal` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `transaksi`
+--
+
+INSERT INTO `transaksi` (`id`, `bank_id`, `tipe`, `jumlah`, `deskripsi`, `tanggal_transaksi`, `tanggal`) VALUES
+(3, 1, 'pemasukan', 10000.00, 'tabungan', '2026-06-22', '2026-06-22 07:20:02'),
+(4, 2, 'pemasukan', 100000.00, 'uang jajan', '2026-06-22', '2026-06-22 07:20:23'),
+(5, 2, 'pengeluaran', 20000.00, 'jajan permen', '2026-06-22', '2026-06-22 07:52:20'),
+(7, 1, 'pemasukan', 5000.00, '', '2026-06-22', '2026-06-22 09:32:13'),
+(8, 1, 'pengeluaran', 5000.00, '', '2026-06-22', '2026-06-22 10:12:59');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `bank_id` (`bank_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD CONSTRAINT `transaksi_ibfk_1` FOREIGN KEY (`bank_id`) REFERENCES `bank` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
